58cf863568c1c65b52a9868c107c9978b88fbab6
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-06-30 18:25:03 +0600

    todo page file setup start

---
+++ src/components/todo/TodoContainer.tsx
@@ -0,0 +1,9 @@
+const TodoContainer = () => {
+  return (
+    <div>
+      <h1>todo Container</h1>
+    </div>
+  );
+};
+
+export default TodoContainer;
---
+++ src/pages/todo.tsx
@@ -0,0 +1,9 @@
+const todo = () => {
+  return (
+    <div>
+      <h1>todo page</h1>
+    </div>
+  );
+};
+
+export default todo;
